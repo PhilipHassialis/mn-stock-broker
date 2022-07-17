8c9c5b6f11f9451d4d31a25691eac4572527179a
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-07-17 15:28:36 +0300

    Finalize Controller Implementation

--- src/main/java/com/philiphassialis/broker/data/InMemoryAccountStore.java
+++ src/main/java/com/philiphassialis/broker/data/InMemoryAccountStore.java
@@ -1,46 +1,56 @@
 package com.philiphassialis.broker.data;
 
-import java.math.BigDecimal;
-import java.util.*;
-
+import com.philiphassialis.broker.Symbol;
 import com.philiphassialis.broker.wallet.DepositFiatMoney;
 import com.philiphassialis.broker.wallet.Wallet;
+import com.philiphassialis.broker.wallet.WithdrawFiatMoney;
 import com.philiphassialis.broker.watchlist.WatchList;
-
 import jakarta.inject.Singleton;
+
+import java.math.BigDecimal;
+import java.util.*;
 
 @Singleton
 public class InMemoryAccountStore {
   public static final UUID ACCOUNT_ID = UUID.fromString("d27fe93c-0066-49ac-9f56-4980ade9e6e3");
   private final HashMap<UUID, WatchList> watchListsPerAccount = new HashMap<>();
   private final Map<UUID, Map<UUID, Wallet>> walletsPerAccount = new HashMap<>();
-
+  
   public WatchList getWatchList(final UUID accountId) {
     return watchListsPerAccount.getOrDefault(accountId, new WatchList());
   }
-
+  
   public WatchList updateWatchList(final UUID accountId, final WatchList watchList) {
     watchListsPerAccount.put(accountId, watchList);
     return getWatchList(accountId);
   }
+  
   public void deleteWatchList(final UUID accountId) {
     watchListsPerAccount.remove(accountId);
   }
-
+  
   public Collection<Wallet> getWallets(final UUID accountId) {
     return Optional.ofNullable(walletsPerAccount.get(accountId)).orElse(new HashMap<>()).values();
   }
-
-
+  
+  
   public Wallet depositToWallet(DepositFiatMoney deposit) {
-    final var wallets = Optional.ofNullable(walletsPerAccount.get(deposit.accountId()))
-            .orElse(new HashMap<>());
-    var oldWallet = Optional.ofNullable(wallets.get(deposit.walletId()))
-            .orElse(new Wallet(ACCOUNT_ID, deposit.walletId(), deposit.symbol(), BigDecimal.ZERO, BigDecimal.ZERO));
-    var newWallet = oldWallet.addAvailable(deposit.amount());
+    return addAvailableToWallet(deposit.accountId(), deposit.walletId(), deposit.symbol(), deposit.amount());
+  }
+  
+  public Wallet withdrawFromWallet(WithdrawFiatMoney withdraw) {
+    return addAvailableToWallet(withdraw.accountId(), withdraw.walletId(), withdraw.symbol(), withdraw.amount());
+  }
+  
+  private Wallet addAvailableToWallet(UUID accountId, UUID walletId, Symbol symbol, BigDecimal changeAmount) {
+    final var wallets = Optional.ofNullable(walletsPerAccount.get(accountId))
+      .orElse(new HashMap<>());
+    var oldWallet = Optional.ofNullable(wallets.get(walletId))
+      .orElse(new Wallet(ACCOUNT_ID, walletId, symbol, BigDecimal.ZERO, BigDecimal.ZERO));
+    var newWallet = oldWallet.addAvailable(changeAmount);
     // update wallet in memory store
-    wallets.put(deposit.walletId(), newWallet);
-    walletsPerAccount.put(deposit.accountId(), wallets);
+    wallets.put(walletId, newWallet);
+    walletsPerAccount.put(accountId, wallets);
     return newWallet;
   }
 }
--- src/main/java/com/philiphassialis/broker/wallet/WalletController.java
+++ src/main/java/com/philiphassialis/broker/wallet/WalletController.java
@@ -4,7 +4,6 @@ import com.philiphassialis.broker.api.RestApiResponse;
 import com.philiphassialis.broker.data.InMemoryAccountStore;
 import com.philiphassialis.broker.wallet.error.CustomError;
 import com.philiphassialis.broker.wallet.error.FiatCurrencyNotSupportedException;
-import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
@@ -22,29 +21,32 @@ import static com.philiphassialis.broker.data.InMemoryAccountStore.ACCOUNT_ID;
 
 @Controller("/account/wallets")
 public record WalletController(InMemoryAccountStore store) {
-    public static final List<String> SUPPORTED_FIAT_CURRENCIES = List.of("EUR", "USD", "GBP", "CHF");
-    private static final Logger LOG = LoggerFactory.getLogger(WalletController.class);
-
-
-    @Get(produces = MediaType.APPLICATION_JSON)
-    public Collection<Wallet> get() {
-        return store.getWallets(ACCOUNT_ID);
+  public static final List<String> SUPPORTED_FIAT_CURRENCIES = List.of("EUR", "USD", "GBP", "CHF");
+  private static final Logger LOG = LoggerFactory.getLogger(WalletController.class);
+  
+  
+  @Get(produces = MediaType.APPLICATION_JSON)
+  public Collection<Wallet> get() {
+    return store.getWallets(ACCOUNT_ID);
+  }
+  
+  @Post(value = "/deposit", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+  public HttpResponse<RestApiResponse> depositFiatMoney(@Body DepositFiatMoney deposit) {
+    if (!SUPPORTED_FIAT_CURRENCIES.contains(deposit.symbol().value()))
+      return HttpResponse.badRequest().body(new CustomError(HttpStatus.BAD_REQUEST.getCode(), "UNSUPPORTED_FIAT_CURRENCY", String.format("Supported currencies %s", SUPPORTED_FIAT_CURRENCIES)));
+    var wallet = store.depositToWallet(deposit);
+    LOG.debug("Wallet {} after deposit {}", wallet, deposit.amount());
+    return HttpResponse.ok(wallet);
+  }
+  
+  @Post(value = "/withdraw", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
+  public Wallet withdrawFiatMoney(@Body WithdrawFiatMoney withdraw) {
+    if (!SUPPORTED_FIAT_CURRENCIES.contains(withdraw.symbol().value())) {
+      throw new FiatCurrencyNotSupportedException(String.format("Supported currencies %s", SUPPORTED_FIAT_CURRENCIES));
     }
-
-    @Post(value = "/deposit", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
-    public HttpResponse<RestApiResponse> depositFiatMoney(@Body DepositFiatMoney deposit) {
-        if (!SUPPORTED_FIAT_CURRENCIES.contains(deposit.symbol().value()))
-            return HttpResponse.badRequest().body(new CustomError(HttpStatus.BAD_REQUEST.getCode(), "UNSUPPORTED_FIAT_CURRENCY", String.format("Supported currencies %s", SUPPORTED_FIAT_CURRENCIES)));
-        var wallet = store.depositToWallet(deposit);
-        LOG.debug("Deposit {} to wallet {}", deposit.amount(), wallet);
-        return HttpResponse.ok(wallet);
-    }
-
-    @Post(value = "/withdraw", consumes = MediaType.APPLICATION_JSON, produces = MediaType.APPLICATION_JSON)
-    public void withdrawFiatMoney(@Body WithdrawFiatMoney withdraw) {
-        if (!SUPPORTED_FIAT_CURRENCIES.contains(withdraw.symbol().value())) {
-            throw new FiatCurrencyNotSupportedException(String.format("Supported currencies %s", SUPPORTED_FIAT_CURRENCIES));
-        }
-    }
-
+    var wallet = store.withdrawFromWallet(withdraw);
+    LOG.debug("Wallet {} after withdraw {}", wallet, withdraw.amount());
+    return wallet;
+  }
+  
 }
